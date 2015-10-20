df0ae7ca6526302d2d40e32e7f4f542d676713b9
sahanasubbanna <sahana.sam@gmail.com>
2015-10-20 13:40:56 -0400

    Template for access.js

---
+++ Assignment1/config/access.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let twitterkeys = {
+	consumerKey: "",
+	consumerSecret: "",
+	callbackURL: "", //Optional
+	accessToken: "",
+	accessTokenSecret: ""
+}
+
+module.exports = twitterkeys;
