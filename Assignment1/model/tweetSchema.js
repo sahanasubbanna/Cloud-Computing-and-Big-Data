9bbeb3833d424453f048be7c007d5023a3063322
Sahana Subbanna <ss4737@columbia.edu>
2015-10-20 20:53:57 -0400

    Basic socket io functionality

--- Assignment1/model/tweetSchema.js
+++ Assignment1/model/tweetSchema.js
@@ -1,16 +1,58 @@
 "use strict";
 
-const mongoose = require('mongoose');
-const twit = require('twit');
+//Using dynamoose for AWS dynamoDB
+let dynamoose = require('dynamoose');
+let keys = require('./../config/access');
 
-let tweetSchema = new mongoose.Schema({
-	internal_id: { type: Number, unique: true, required: true}, //autoincremented
+
+dynamoose.AWS.config.update({
+  accessKeyId: keys.awsKeys.accessKey,
+  secretAccessKey: keys.awsKeys.accessKeySecret,
+  region: 'us-east-1'
+});
+
+
+let tweetSchema = new dynamoose.Schema({
+	internal_id: { 
+		type: Number, 
+		unique: true, 
+		required: true, 
+		validate: function(v) { return v > 0; },
+    	hashKey: true
+    }, //autoincremented
 	tweet_id: {type: Number, unique: true, required: true},
 	twitterHandle: { type: String, unique: true, required: true}, 
 	latLong: {type: Number, required: true},
 	tags: [String]
 });
 
-let tweetCollection = mongoose.model('tweets', tweetSchema);
 
-module.exports = { tweetSchema, tweetCollection };
+let options = {
+  create: true, // Create table in DB, if it does not exist
+  waitForActive: true, // Wait for table to be created before trying to use it
+  waitForActiveTimeout: 180000 // wait 3 minutes for table to activate
+}
+
+let tweetCollection = dynamoose.model('tweets', tweetSchema, options);
+
+module.exports = { tweetCollection, tweetSchema };
+
+
+
+//------------------------------------------------------------------------------------------
+//Using Mongoose
+// const mongoose = require('mongoose');
+// const twit = require('twit');
+
+// let tweetSchema = new mongoose.Schema({
+// 	internal_id: { type: Number, unique: true, required: true}, //autoincremented
+// 	tweet_id: {type: Number, unique: true, required: true},
+// 	twitterHandle: { type: String, unique: true, required: true}, 
+// 	latLong: {type: Number, required: true},
+// 	tags: [String]
+// });
+
+// let tweetCollection = mongoose.model('tweets', tweetSchema);
+
+// module.exports = { tweetSchema, tweetCollection };
+//-----------------------------------------------------------------------------------------
--- Assignment1/server.js
+++ Assignment1/server.js
@@ -9,11 +9,11 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const autoincrement = require('mongoose-auto-increment');
 const twit = require('twit');
-const twitterAccess = require('./config/access');
+const keys = require('./config/access');
 const database = require('./config/database');
 const tweetModel = require('./model/tweetSchema');
 const twitterStreamRoutes = require('./routes/twitterStreamRoutes');
-
+const path = require('path');
 
 let app = express();
 
@@ -26,13 +26,50 @@ app.use(logger('Tweets!!!'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-
-//Connect to DB
-
-app.use('/tweetsgeomap', twitterStreamRoutes);
+//app.use('/tweetsgeomap', twitterStreamRoutes);
 
 var server = http.createServer(app).listen(app.get('port'), () => {
 	console.log(`Tweet Server started on port: ${app.get('port')}`); 
 });
 
+//Twitter
+var T = new twit({
+  consumer_key: keys.twitterKeys.consumerKey,
+  consumer_secret: keys.twitterKeys.consumerSecret,
+  access_token: keys.twitterKeys.accessToken,
+  access_token_secret: keys.twitterKeys.accessTokenSecret
+})
 
+//Socket.io
+var io = require('socket.io').listen(server);
+var stream = T.stream('statuses/sample');
+
+io.on('connection', function (socket) {
+  stream.on('tweet', function(tweet) {
+  	// console.log(tweet);
+    socket.emit('info', { tweet: tweet});
+  });
+});
+
+
+
+//Connect to DB
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, './views', 'index.html'));
+});
+
+// // Send current time to all connected clients
+// function sendTime() {
+//     io.emit('time', { time: new Date().toJSON() });
+// }
+
+// Send current time every 10 secs
+// setInterval(sendTime, 10000);
+
+// Emit welcome message on connection
+io.on('connection', function(socket) {
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+
+    socket.on('i am client', console.log);
+});
